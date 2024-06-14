c9fa0491fe3682554e8cd0293d3e8eb5c43a9368
Aniket1703 <aniketpatil17031999@gmail.com>
2024-06-14 21:58:54 +0530

    Prepare app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from  "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -20,8 +21,9 @@ cloudinary.config({
 
 const app = express();
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
 app.use(cookieParser());
 
@@ -31,6 +33,14 @@ app.use("/api/user",userRoute);
 app.use("/api/posts",postRoute);
 app.use("/api/notifications",notificationRoute);
 
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 app.listen(port,()=>{
